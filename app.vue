0091a0e346665e5c29f7b18f3d6ddce3e84546df
Shamimzz <shamimislam5091@gmail.com>
2022-09-16 12:40:43 +0600

    added

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Baby name generator</h1>
+    <p>Choose your option</p>
+    <div class="container">
+     
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+  
+</style>
