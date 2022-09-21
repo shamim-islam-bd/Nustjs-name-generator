d51b9e5be7bdb6d0863c054b2ed7055c89eaa8df
Shamimzz <shamimislam5091@gmail.com>
2022-09-21 11:57:05 +0600

    page setup

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <Nav />
+  </div>
+  <div class="container text-center mt-5">
+    <h1>WelCome to Restaurent</h1>
+    <a href="/restaurants">Go to Restaurent</a>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/restaurants.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container mt-5">
+      <h1>This is Restaureents</h1>
+    </div>
+  </div>
+</template>
